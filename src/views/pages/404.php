91bb167f292b83f663bf15dd608dc7e9facfe9e4
Renan Gustavo <renandevfullstack@gmail.com>
2023-08-27 11:16:33 -0300

    404 page

---
+++ src/views/pages/404.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página não encontrada</title>
+
+    <link rel="stylesheet" href="<?= $baseUrl ?>/assets/css/404.css" />
+</head>
+<body>
+    <h1>404 Error Page #3</h1>
+    <section class="error-container">
+        <span>4</span>
+        <span><span class="screen-reader-text">0</span></span>
+        <span>4</span>
+    </section>
+    <div class="link-container">
+        <a href="javascript:history.back()" class="more-link">Voltar</a>
+    </div>
+</body>
+</html>
