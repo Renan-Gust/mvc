7797c7f364165b07da79a8d841ee517904643edb
Renan Gustavo <renandevfullstack@gmail.com>
2023-10-28 14:06:56 -0300

    .env

--- public/index.php
+++ public/index.php
@@ -3,4 +3,7 @@ session_start();
 require '../vendor/autoload.php';
 require '../src/routes.php';
 
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
+$dotenv->load();
+
 $router->run( $router->routes );
